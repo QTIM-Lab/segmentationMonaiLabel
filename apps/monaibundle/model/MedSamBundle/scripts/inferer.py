55ee33d6837b87108594406dc8d4551f65946888
bbearce <bbearce@gmail.com>
2024-12-11 16:35:05 -0700

    advaith's oct model

--- apps/monaibundle/model/MedSamBundle/scripts/inferer.py
+++ apps/monaibundle/model/MedSamBundle/scripts/inferer.py
@@ -12,29 +12,41 @@ class MedSamInferer(SimpleInferer):
     def __init__(self) -> None:
         super().__init__()
         # Load the MedSAM model
+        # pdb.set_trace()
         self._model = SamModel.from_pretrained("flaviagiammarino/medsam-vit-base", local_files_only=False)
         self._processor = SamProcessor.from_pretrained("flaviagiammarino/medsam-vit-base")
-
-    def forward(self, inputs, device, trained=False):
+        model_ckpt_path = "/monailabel/apps/monaibundle/model/MedSamBundle/models/advaith_epoch=30-step=961.ckpt"
         # pdb.set_trace()
-        if trained:
-            model_ckpt_path = "/home/bearceb/Documents/OPTIMEyes/monailabel/apps/monaibundle/model/MedSamBundle/models/epoch=20-step=210.ckpt"
-            raw_torch_load = torch.load(model_ckpt_path)
-             # Create a new state dictionary with updated keys
-            new_state_dict = {key.replace("model.", ""): value for key, value in raw_torch_load['state_dict'].items()}
-            self._model.load_state_dict(state_dict = new_state_dict, strict=True)
-        else:
-            # pdb.set_trace()
-            self._model = SamModel.from_pretrained("flaviagiammarino/medsam-vit-base", local_files_only=False)
-            # Or load from local
-            ## Archive way to save but is handled in README.md at root of OPTIMEyes
-            # torch.save(self._model.state_dict(), '/monailabel/apps/monaibundle/model/MedSamBundle/models/model.pt')
-            ## Use this to load from disk
-            # loaded_weights = torch.load('/monailabel/apps/monaibundle/model/MedSamBundle/models/model.pt', weights_only=True)
-            # self._model.load_state_dict(raw_torch_load)
+        raw_torch_load = torch.load(model_ckpt_path, map_location=torch.device('cuda'))
+        # Create a new state dictionary with updated keys
+        new_state_dict = {key.replace("model.", ""): value for key, value in raw_torch_load['state_dict'].items()}
+        self._model.load_state_dict(state_dict = new_state_dict, strict=True)
+        self._model.to('cuda') 
 
-            
-        self._model.to(device) 
+
+
+    def forward(self, inputs, device, trained=True):
+        # if trained:
+        #     print("""
+
+        #     TRAINED
+
+        #     """)
+        #     # model_ckpt_path = "/monailabel/apps/monaibundle/model/MedSamBundle/models/epoch=20-step=210.ckpt"
+        #     model_ckpt_path = "/monailabel/apps/monaibundle/model/MedSamBundle/models/advaith_epoch=30-step=961.ckpt"
+        #     # pdb.set_trace()
+        #     raw_torch_load = torch.load(model_ckpt_path, map_location=torch.device('cuda'))
+        #      # Create a new state dictionary with updated keys
+        #     new_state_dict = {key.replace("model.", ""): value for key, value in raw_torch_load['state_dict'].items()}
+        #     self._model.load_state_dict(state_dict = new_state_dict, strict=True)
+        # else:
+        #     print("""
+
+        #     NOT TRAINED
+
+        #     """)
+        #     self._model = SamModel.from_pretrained("flaviagiammarino/medsam-vit-base")
+        # pdb.set_trace()
         input_instance = inputs[0]
         pv = input_instance["pixel_values"]
         ib = input_instance["input_boxes"]
@@ -44,24 +56,36 @@ class MedSamInferer(SimpleInferer):
         # pv = input_instance["pixel_values"].reshape((1, 3, 1024, 1024))
         # ib = input_instance["input_boxes"].reshape(1, 1, 4)
         outputs = self._model(pixel_values=pv.to(device), input_boxes=ib.to(device), multimask_output=False)
+        ### AV
         logits = outputs.pred_masks
-        l_sig = logits.sigmoid().cpu()
-        logits.squeeze().max()
         o_s = input_instance["original_sizes"].cpu()
         r_i_s = input_instance["reshaped_input_sizes"].cpu()
+        logits_resized = self._processor.image_processor.post_process_masks(logits, o_s, r_i_s, binarize=False)
         # pdb.set_trace()
-        probs = self._processor.image_processor.post_process_masks(l_sig, o_s, r_i_s, binarize=False)
-        binary_mask = (probs[0] > 0.45).int() * 255
+        probs = [l.sigmoid().cpu() for l in logits_resized]
+        # logits_resized.squeeze().max()
+        ### AV
+        ### BB
+        # logits = outputs.pred_masks
+        # l_sig = logits.sigmoid().cpu()
+        # logits.squeeze().max()
+        # o_s = input_instance["original_sizes"].cpu()
+        # r_i_s = input_instance["reshaped_input_sizes"].cpu()
+        # # pdb.set_trace()
+        # probs = self._processor.image_processor.post_process_masks(l_sig, o_s, r_i_s, binarize=False)
+        ### BB
+        binary_mask = (probs[0] > 0.50).int() * 255
         tmp = Image.fromarray(np.uint8(np.array(binary_mask.squeeze()))).convert('RGB')
-        # tmp.save("/monailabel/apps/deleteme/tmp_inferer.png")
-        max(probs[0])
-        np.unique(binary_mask)
+        # tmp.save("/scratch90/bb-10_25_2024/segmentationMonaiLabel/tmp.png")
+        # max(probs[0])
+        # np.unique(binary_mask)
         # np.unique(probs)
-
         # tmp = Image.fromarray(np.uint8(np.array(binary_mask.squeeze()))).convert('RGB')
         # tmp.save("/sddata/projects/segmentationMonaiLabel/tmp_inferer.png")
 
         return binary_mask
+
+
 
     def __call__(self, inputs, network=None, *args, **kwargs):
         """Unified callable function API of Inferers.
@@ -74,9 +98,10 @@ class MedSamInferer(SimpleInferer):
             kwargs: optional keyword args to be passed to ``network``.
 
         """
-        if network is not None:
-            print("LOADING model_best.pt")
-            self._model.load_state_dict(torch.load(network))
+        # if network is not None:
+        #     pdb.set_trace()
+        #     print("LOADING model_best.pt")
+        #     self._model.load_state_dict(torch.load(network))
         device = kwargs.get('device', None) if kwargs else 'cpu'
         print("Device:", device)
         # pdb.set_trace()
